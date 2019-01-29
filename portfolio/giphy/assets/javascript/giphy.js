2feeaf7033bc404e0481e444b351c0cfb96ef50c
JJ <jenjones@ucdavis.edu>
2019-01-28 18:16:49 -0800

    update giphy app

--- portfolio/giphy/assets/javascript/giphy.js
+++ portfolio/giphy/assets/javascript/giphy.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
-    // array for initial buttons
+    var offsetNum = 0;
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=UlsGZGJN2JtxEIbf0FazuLxh7m2ASR2x";
     var topics = ["Basketball", "Bowling", "Boxing", "Golf", "Skiing", "Karate", "Rowing", "Swimming", "Tennis", "Volleyball"];
 
     // function to dynamically create buttons by looping through the array
@@ -33,8 +34,10 @@ $(document).ready(function () {
     function displayImages() {
         $("#images").empty();
         var sport = $(this).attr("data-name");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=UlsGZGJN2JtxEIbf0FazuLxh7m2ASR2x&q=" + sport + "+fail&rating=&limit=10";
-        var offsetNum = 0;
+
+        queryURL = "https://api.giphy.com/v1/gifs/search?api_key=UlsGZGJN2JtxEIbf0FazuLxh7m2ASR2x";
+        offsetNum = 0;
+        queryURL = queryURL + "&q=" + sport + "+fail&rating=&limit=10";
 
         $.ajax({
             url: queryURL,
@@ -42,68 +45,58 @@ $(document).ready(function () {
         }).then(function (response) {
             console.log(response);
 
-            // for loop through the returned GIFs, dynamically insert them, and display title, source, and rating
-            for (var i = 0; i < response.data.length; i++) {
-                var sportDiv = $("<div>");
-                // var p = $("<p>").text("Title:  " + response.data[i].title + ", " + "Source: " + response.data[i].source_tld + ", " + "Rating: " + response.data[i].rating);          
-                var t = $("<p>").text("Title:  " + response.data[i].title);
-                var s = $("<p>").text("Source: " + response.data[i].source_tld);
-                var r = $("<p>").text("Rating: " + response.data[i].rating);            
-                sportDiv.append(t,s,r);
-                var image = $("<img data-state='still' class='sport mb-5'>").attr("src", response.data[i].images.fixed_height_still.url);
-                image.attr("data-still", response.data[i].images.fixed_height_still.url);
-                image.attr("data-animate", response.data[i].images.fixed_height.url);
-                sportDiv.append(image);
-                $("#images").append(sportDiv);
-            }
-
-            // function for on click event to change GIF from still to animated
-            function state() {
-                $(".sport").on("click", function () {
-                    console.log("click");
-                    var state = $(this).attr("data-state");
-                    if (state === "still") {
-                        var animateURL = $(this).attr("data-animate");
-                        $(this).attr('src', animateURL);
-                        $(this).attr('data-state', 'animate');
-                    } else {
-                        $(this).attr('src', $(this).attr('data-still'));
-                        $(this).attr('data-state', "still");
-                    }
-                });
-            }
-            // run function for initial 10 GIFs
-            state();
-
-            // on click event to run displayMoreImages function
-            $(document).on("click", ".moreImages", displayMoreImages);
-
-            // function to append 10 more GIFs to page, different images called each time function runs
-            function displayMoreImages() {
-                offsetNum += 10;
-                console.log(offsetNum);
-
-                var queryUrlAgain = queryURL + "&offset=" + offsetNum;
-                console.log(queryUrlAgain);
-
-                $.ajax({
-                    url: queryUrlAgain,
-                    method: "GET"
-                }).then(function (response) {
-                    for (var i = 0; i < response.data.length; i++) {
-                        var sportDiv = $("<div>");
-                        var p = $("<p>").text("Title:  " + response.data[i].title + ", " + "Source: " + response.data[i].source_tld + ", " + "Rating: " + response.data[i].rating);
-                        sportDiv.append(p);
-                        var image = $("<img data-state='still' class='sport mb-5'>").attr("src", response.data[i].images.fixed_height_still.url);
-                        image.attr("data-still", response.data[i].images.fixed_height_still.url);
-                        image.attr("data-animate", response.data[i].images.fixed_height.url);
-                        sportDiv.append(image);
-                        $("#images").append(sportDiv);
-                    }
-                    // run function for newly added GIFs
-                    state();
-                });
-            }
+            generateImagesToView(response.data)
         })
     };
-})
+
+    // function to create images
+    function generateImagesToView(data) {
+        for (var i = 0; i < data.length; i++) {
+            var sportDiv = $("<div>");
+            // var p = $("<p>").text("Title:  " + data[i].title + ", " + "Source: " + data[i].source_tld + ", " + "Rating: " + data[i].rating);
+            var t = $("<p>").text("Title:  " + data[i].title);
+            var s = $("<p>").text("Source: " + data[i].source_tld);
+            var r = $("<p>").text("Rating: " + data[i].rating);
+            sportDiv.append(t, s, r);
+            var image = $("<img data-state='still' class='sport mb-5'>").attr("src", data[i].images.fixed_height_still.url);
+            image.attr("data-still", data[i].images.fixed_height_still.url);
+            image.attr("data-animate", data[i].images.fixed_height.url);
+            sportDiv.append(image);
+
+            $("#images").prepend(sportDiv);
+        }
+    }
+
+    // on click event to change image state between still to animate
+    $("#images").on("click", 'div > img', function () {
+        console.log("click");
+        var state = $(this).attr("data-state");
+        if (state === "still") {
+            var animateURL = $(this).attr("data-animate");
+            $(this).attr('src', animateURL);
+            $(this).attr('data-state', 'animate');
+        } else {
+            $(this).attr('src', $(this).attr('data-still'));
+            $(this).attr('data-state', "still");
+        }
+    });
+
+    // on click event to run displayMoreImages function
+    $(document).on("click", ".moreImages", displayMoreImages);
+
+    // function to append 10 more GIFs to page, different images called each time function runs
+    function displayMoreImages() {
+        offsetNum += 10;
+        console.log(offsetNum);
+
+        var queryUrlAgain = queryURL + "&offset=" + offsetNum;
+        console.log(queryUrlAgain);
+
+        $.ajax({
+            url: queryUrlAgain,
+            method: "GET"
+        }).then(function (response) {
+            generateImagesToView(response.data)
+        });
+    }
+});
