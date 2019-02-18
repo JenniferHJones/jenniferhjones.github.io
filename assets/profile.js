ac6c312ad1ded9de154dc7e96609d465b59198b3
JJ <jenjones@ucdavis.edu>
2019-02-18 14:31:48 -0800

    added contact modal

--- assets/profile.js
+++ assets/profile.js
@@ -1,5 +1,12 @@
 $(document).ready(function () {
 
+    document.getElementById("email").addEventListener("click", function() {
+        document.querySelector(".bg-modal").style.display = "flex";
+    });
+
+    document.querySelector(".close").addEventListener("click", function() {
+        document.querySelector(".bg-modal").style.display = "none";
+    });
 
     
 })
