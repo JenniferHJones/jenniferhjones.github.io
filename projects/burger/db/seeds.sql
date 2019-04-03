e74a54c5835774737a49b353dd7cd3d92798cb89
JJ <jenjones@ucdavis.edu>
2019-04-03 09:31:07 -0700

    removing burger app

--- projects/burger/db/seeds.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO burgers (burger_name) VALUES ("Veggie Burger");
-INSERT INTO burgers (burger_name) VALUES ("Turkey Burger");
-INSERT INTO burgers (burger_name) VALUES ("Black Bean Burger");
-INSERT INTO burgers (burger_name) VALUES ("Bacon Double Cheese Burger");
-
